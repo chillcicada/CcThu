51ac5797120a174216bac54b15738580a88d3cd4
chillcicada <2210227279@qq.com>
2024-06-30 01:40:46 +0800

    feat: prune srun/base64encode

--- src/utils/srun.ts
+++ src/utils/srun.ts
@@ -59,75 +59,10 @@ export function xEncode(str: string, key: string): string {
 /**
  * A minimal modified base64 encoder/decoder
  */
-export function xEncodeBase64(str: string): string {
-  const rule = 'LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE'
-  const padding = '='
 
-  const len = str.length
-  let res = ''
+export function xEncodeBase64(str: string) {
+  const _base64Alphabet = 'LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE45QA'
+  const base64Alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 
-  for (let i = 0; i < len; i += 3) {
-    const h = str.charCodeAt(i) << 16 | (i + 1 < len ? str.charCodeAt(i + 1) << 8 : 0) | (i + 2 < len ? str.charCodeAt(i + 2) : 0)
-
-    for (let j = 0; j < 4; j += 1)
-      i * 8 + j * 6 > len * 8 ? res += padding : res += rule.charAt(h >>> 6 * (3 - j) & 63)
-  }
-
-  return res
+  return btoa(str).split('').map(c => c === '=' ? '=' : _base64Alphabet.charAt(base64Alphabet.indexOf(c))).join('')
 }
-
-// with decode as a class
-// export class XBase64 {
-//   private n = 'LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE'
-//   private r = '='
-
-//   public encode(t: string): string {
-//     const a = t.length
-//     let u = ''
-
-//     for (let o = 0; o < a; o += 3) {
-//       const h = t.charCodeAt(o) << 16 | (o + 1 < a ? t.charCodeAt(o + 1) << 8 : 0) | (o + 2 < a ? t.charCodeAt(o + 2) : 0)
-
-//       for (let i = 0; i < 4; i += 1)
-//         o * 8 + i * 6 > a * 8 ? u += this.r : u += this.n.charAt(h >>> 6 * (3 - i) & 63)
-//     }
-
-//     return u
-//   }
-
-//   public decode(e: string): string {
-//     if (!e)
-//       return e
-
-//     let o = 0
-//     let C = 0
-
-//     const s: string[] = []
-
-//     e = e.replace(new RegExp(`\\${this.r}`, 'gi'), '')
-//     do {
-//       const a = this.n.indexOf(e.charAt(o += 1))
-//       const c = this.n.indexOf(e.charAt(o += 1))
-//       const l = this.n.indexOf(e.charAt(o += 1))
-//       const D = this.n.indexOf(e.charAt(o += 1))
-
-//       const B = a << 18 | c << 12 | l << 6 | D
-//       const i = B >> 16 & 255
-//       const h = B >> 8 & 255
-//       const u = B & 255
-//       C += 1
-
-//       if (l === 64) {
-//         s[C] = String.fromCharCode(i)
-//       }
-//       else if (D === 64) {
-//         s[C] = String.fromCharCode(i, h)
-//       }
-//       else {
-//         s[C] = String.fromCharCode(i, h, u)
-//       }
-//     } while (o < e.length)
-
-//     return s.join('')
-//   }
-// }
